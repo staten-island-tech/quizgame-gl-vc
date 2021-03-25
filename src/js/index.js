4dc8b96550d996bf1f5f4a08c03d48e6f2c3eef5
gracel202 <linlgrace556@gmail.com>
2021-03-25 00:14:17 -0400

    changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
 console.log("connected");
+
+//quiz question array
+
+//question object
+
+//listen for start event, click, page load
+
+//display question or questions
+
+//listen for user answer click
+
+//check if choice is correct
+//if answer is true then update score show correct modal/green check
+//else update score show false modal/red x
+
+//if question number is less than questions.Length show next question
+//else display results
