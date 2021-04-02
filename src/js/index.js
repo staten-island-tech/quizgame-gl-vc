3187d079808416d5e53a4df105b790939ae6687c
vickyc1 <71679303+vickyc1@users.noreply.github.com>
2021-04-02 11:03:00 -0700

    hi

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,39 @@ console.log("connected");
 
 //question object
 
+const questions = [
+  {
+    question: "What is a tomato?",
+    choices: {
+      a: "A vegetable",
+      b: "A fruit",
+      c: "Both A and B",
+      d: "None of the above",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "What is a mule the offspring of?",
+    choices: {
+      a: "A male horse & a female donkey",
+      b: "A female horse & a male donkey",
+      c: "2 mules",
+      d: "Magic",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question: "What is 9 + 10?",
+    choices: {
+      a: "19",
+      b: "21",
+      c: "Both 19 & 21",
+      d: "Only a genius can solve this",
+    },
+    correctAnswer: "b",
+  },
+];
+
 //listen for start event, click, page load
 
 //display question or questions
@@ -16,3 +49,5 @@ console.log("connected");
 
 //if question number is less than questions.Length show next question
 //else display results
+
+var score = 0;
