07a3d6092425d216f09997252a4a21c90fb399ba
gracel202 <linlgrace556@gmail.com>
2021-04-12 15:45:01 -0400

    uh yea

---
+++ src/js/questions.js
@@ -0,0 +1,42 @@
+const questionData = [
+  {
+    question: "What is a tomato?",
+    a: "vegetable",
+    b: "fruit",
+    c: "Both A and B",
+    d: "None of the above",
+    correctChoice: "b",
+  },
+  {
+    question: "What is a mule the offspring of?",
+    a: "male horse & female donkey",
+    b: "female horse & male donkey",
+    c: "2 mules",
+    d: "Magic",
+    correctChoice: "b",
+  },
+  {
+    question: "What is 9 + 10?",
+    a: "19",
+    b: "21",
+    c: "19 & 21",
+    d: "Only a genius can solve this",
+    correctChoice: "b",
+  },
+  {
+    question: "Where is SITHS located?",
+    a: "458 Clawson Ave",
+    b: "458 Clawsson St",
+    c: "485 Clawson St",
+    d: "485 Clawson Rd",
+    correctChoice: "c",
+  },
+  {
+    question: "What is the powerhouse of a cell?",
+    a: "vacuole",
+    b: "lysosome",
+    c: "ribosomes",
+    d: "mitochondria",
+    correctChoice: "d",
+  },
+];
