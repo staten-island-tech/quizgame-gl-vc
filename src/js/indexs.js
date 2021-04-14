30c07873fd773ed9153a6786fb9960eda1acd30c
vickyc1 <71679303+vickyc1@users.noreply.github.com>
2021-04-13 19:58:10 -0700

    update

--- src/js/indexs.js
+++ src/js/indexs.js
@@ -4,59 +4,6 @@ import { myQuestions } from "./questions";
 
 //immediately invoked function
 (function () {
-  const myQuestions = [
-    {
-      question: "What is a tomato?",
-      answers: {
-        a: "vegetable",
-        b: "fruit",
-        c: "Both A and B",
-        d: "None of the above",
-      },
-      correctAnswer: "b",
-    },
-    {
-      question: "What is a mule the offspring of?",
-      answers: {
-        a: "male horse & female donkey",
-        b: "female horse & male donkey",
-        c: "2 mules",
-        d: "magic",
-      },
-      correctAnswer: "b",
-    },
-    {
-      question: "What is 9 + 10?",
-      answers: {
-        a: "19",
-        b: "21",
-        c: "19 & 21",
-        d: "Only a genius can solve this",
-      },
-      correctAnswer: "b",
-    },
-    {
-      question: "Where is SITHS located?",
-      answers: {
-        a: "458 Clawson Ave",
-        b: "458 Clawsson St",
-        c: "485 Clawson St",
-        d: "485 Clawson Rd",
-      },
-      correctAnswer: "c",
-    },
-    {
-      question: "What is the powerhouse of a cell?",
-      answers: {
-        a: "vacuole",
-        b: "lysosome",
-        c: "ribosomes",
-        d: "mitochondria",
-      },
-      correctAnswer: "d",
-    },
-  ];
-
   // Functions
   function buildQuiz() {
     // variable to store the HTML output, including questions and answer choices
--- src/js/questions.js
+++ src/js/questions.js
@@ -1,102 +1,54 @@
-
-  const myQuestions = [
-    {
-      question: "What is a tomato?",
-      answers: {
-        a: "vegetable",
-        b: "fruit",
-        c: "Both A and B",
-        d: "None of the above"
-      },
-      correctAnswer: "b"
-    },
-    {
-      question: "What is a mule the offspring of?",
-      answers: {
-       a: "male horse & female donkey",
-    b: "female horse & male donkey",
-    c: "2 mules",
-    d: "magic",
-      },
-      correctAnswer: "b"
-    },
-    {
-      question: "What is 9 + 10?",
-      answers: {
-        a: "19",
-    b: "21",
-    c: "19 & 21",
-    d: "Only a genius can solve this"
-      },
-      correctAnswer: "b"
-    },
-    {
-      question:  "Where is SITHS located?",
-      answers: {
-        a: "458 Clawson Ave",
-    b: "458 Clawsson St",
-    c: "485 Clawson St",
-    d: "485 Clawson Rd"
-      },
-      correctAnswer: "c"
-    },
-     {
-      question:   "What is the powerhouse of a cell?",
-      answers: {
-        a: "vacuole",
-    b: "lysosome",
-    c: "ribosomes",
-    d: "mitochondria"
-      },
-      correctAnswer: "d"
-     }
-  ];
-
-/*
-const questionData = [
+const myQuestions = [
   {
     question: "What is a tomato?",
-    a: "vegetable",
-    b: "fruit",
-    c: "Both A and B",
-    d: "None of the above",
-    correctChoice: "b",
+    answers: {
+      a: "vegetable",
+      b: "fruit",
+      c: "Both A and B",
+      d: "None of the above",
+    },
+    correctAnswer: "b",
   },
   {
     question: "What is a mule the offspring of?",
-    a: "male horse & female donkey",
-    b: "female horse & male donkey",
-    c: "2 mules",
-    d: "Magic",
-    correctChoice: "b",
+    answers: {
+      a: "male horse & female donkey",
+      b: "female horse & male donkey",
+      c: "2 mules",
+      d: "magic",
+    },
+    correctAnswer: "b",
   },
   {
     question: "What is 9 + 10?",
-    a: "19",
-    b: "21",
-    c: "19 & 21",
-    d: "Only a genius can solve this",
-    correctChoice: "b",
+    answers: {
+      a: "19",
+      b: "21",
+      c: "19 & 21",
+      d: "Only a genius can solve this",
+    },
+    correctAnswer: "b",
   },
   {
     question: "Where is SITHS located?",
-    a: "458 Clawson Ave",
-    b: "458 Clawsson St",
-    c: "485 Clawson St",
-    d: "485 Clawson Rd",
-    correctChoice: "c",
+    answers: {
+      a: "458 Clawson Ave",
+      b: "458 Clawsson St",
+      c: "485 Clawson St",
+      d: "485 Clawson Rd",
+    },
+    correctAnswer: "c",
   },
   {
     question: "What is the powerhouse of a cell?",
-    a: "vacuole",
-    b: "lysosome",
-    c: "ribosomes",
-    d: "mitochondria",
-    correctChoice: "d",
+    answers: {
+      a: "vacuole",
+      b: "lysosome",
+      c: "ribosomes",
+      d: "mitochondria",
+    },
+    correctAnswer: "d",
   },
 ];
-
-export { questionData };
-*/
 
 export { myQuestions };
